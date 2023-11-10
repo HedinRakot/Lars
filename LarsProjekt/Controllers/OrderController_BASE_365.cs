845cc8a9bc0baca9b75eaf93804e5d1fe3c9030f
Lars Ludwig <lars.ldwg@gmail.com>
2023-11-10 17:43:13 +0100

    .

---
+++ LarsProjekt/Controllers/OrderController_BASE_365.cs
@@ -0,0 +1,202 @@
+﻿using LarsProjekt.Application;
+using LarsProjekt.Database;
+using LarsProjekt.Database.Repositories;
+using LarsProjekt.Domain;
+using LarsProjekt.Models;
+using LarsProjekt.Models.Mapping;
+using LarsProjekt.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System.Collections.Generic;
+
+namespace LarsProjekt.Controllers;
+public class OrderController : Controller
+{
+    private readonly IOrderRepository _orderRepository;
+    private readonly ShoppingCartRepository _cartRepository;
+    private readonly IOrderDetailRepository _orderDetailRepository;
+    private readonly IUserRepository _userRepository;
+    private readonly IAddressRepository _addressRepository;
+    private readonly ICouponRepository _couponRepository;
+    private readonly ISqlUnitOfWork _sqlUnitOfWork;
+    //private readonly DiscountPriceRepository _discountPriceRepository;
+
+    public OrderController
+        (IOrderRepository orderRepository,
+        ShoppingCartRepository cartRepository,
+        IOrderDetailRepository orderDetailRepository,
+        IUserRepository userRepository,
+        IAddressRepository addressRepository,
+        ICouponRepository couponRepository,
+        ISqlUnitOfWork sqlUnitOfWork)
+        //DiscountPriceRepository discountPriceRepository)
+    {
+        _orderRepository = orderRepository;
+        _cartRepository = cartRepository;
+        _orderDetailRepository = orderDetailRepository;
+        _userRepository = userRepository;
+        _addressRepository = addressRepository;
+        _couponRepository = couponRepository;
+        _sqlUnitOfWork = sqlUnitOfWork;
+        //_discountPriceRepository = discountPriceRepository;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var user = _userRepository.GetByName(HttpContext.User.Identity.Name);
+        var address = _addressRepository.Get(user.AddressId);
+        var orders = _orderRepository.GetAll();
+        var list = new List<OrderModel>();
+        foreach (var order in orders)
+        {
+            if (user.Id == order.UserId)
+            {                
+                list.Add(order.ToModel());
+                order.Address = address;
+                
+            }                
+        }
+        return View(list);
+    }
+
+    [HttpGet]
+    public IActionResult Checkout()
+    {
+        var user = _userRepository.GetByName(HttpContext.User.Identity.Name);        
+        var model = _addressRepository.Get(user.AddressId).ToModel();        
+        return View(model);
+    }
+
+    [HttpGet]
+    public IActionResult Confirmation()
+    {
+        var list = new List<ShoppingCartItemModel>();
+        foreach (var item in _cartRepository.ShoppingCartItems)
+        {
+            list.Add(new ShoppingCartItemModel
+            {
+                Product = item.Product.ToModel(),
+                Amount = item.Amount
+            });
+        }
+
+        OrderConfirmationVM orderConfirmationVM = new OrderConfirmationVM()
+        {
+            Coupons = new CouponModel(),
+            Items = list,
+            Total = GetTotal()
+        };
+       
+        return View(orderConfirmationVM);
+    }
+
+    [HttpGet]
+    public IActionResult Details(long id)
+    {
+        var detail = _orderDetailRepository.GetListWithOrderId(id);
+        var list = new List<OrderDetailModel>();
+        foreach (var item in detail)
+        {
+            list.Add(new OrderDetailModel
+            {
+                OrderId = item.OrderId,
+                ProductId = item.ProductId,
+                Quantity = item.Quantity,
+                UnitPrice = item.UnitPrice,
+                Id = id
+            });
+        }
+        return View(list);
+    }
+
+
+    [HttpPost]
+    public IActionResult CreateOrder(OrderModel model)
+    {
+        
+        if (ModelState.IsValid)
+        {
+            var user = _userRepository.GetByName(HttpContext.User.Identity.Name);
+
+            // add order
+            var order = model.ToDomain();
+            order.UserId = user.Id;
+            order.AddressId = user.AddressId;
+            order.Address = user.Address;
+            order.Total = GetTotal();
+            _sqlUnitOfWork.OrderRepository.Add(order);
+            _sqlUnitOfWork.SaveChanges();
+
+            //add orderDetail            
+            var cartItems = _cartRepository.ShoppingCartItems.ToList();
+            foreach (var item in cartItems)
+            {
+                var orderDetail = new OrderDetail
+                {
+                    Quantity = item.Amount,
+                    UnitPrice = item.Product.PriceOffer,
+                    ProductId = item.Product.Id,
+                    OrderId = order.Id
+                };
+                _sqlUnitOfWork.OrderDetailRepository.Add(orderDetail);
+                _sqlUnitOfWork.SaveChanges();
+            }            
+
+            // clear cart
+            var items = _cartRepository.ShoppingCartItems.ToList();
+            foreach (var item in items)
+            {
+                _cartRepository.ShoppingCartItems.Remove(item);
+            }
+
+        }
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    public IActionResult Redeem(OrderConfirmationVM model)
+    {
+        var coupons = _couponRepository.GetAll();       
+
+        foreach (var coupon in coupons)
+        {
+            if (model.UserInput == coupon.Code)
+            {
+                var discount = coupon.Discount;
+                decimal.TryParse(discount, out decimal x);                
+                var totalDiscount = (x / 100) * model.Total;                
+                var newTotalPrice = model.Total - totalDiscount;
+                model.Total = newTotalPrice;
+
+                //_discountPriceRepository.Price.PriceAfterDiscount = newTotalPrice;
+
+                return Json(new { success = "true", model });
+            }
+        }
+        return View();
+    }
+
+    //[HttpGet]
+    //public IActionResult Redeem()
+    //{
+    //    return Ok(new { success = "true", newPrice = _discountPriceRepository.Price.PriceAfterDiscount });
+    //}
+
+    public IActionResult Payment()
+    {
+        return View();
+    }
+
+    private decimal? GetTotal()
+    {
+        decimal? total = (from cartItems in _cartRepository.ShoppingCartItems
+                          select cartItems.Amount *
+                          cartItems.Product.PriceOffer).Sum();
+
+        return total ?? decimal.Zero;
+    }
+
+}
+
+
---
+++ LarsProjekt/Controllers/OrderController_LOCAL_365.cs
@@ -0,0 +1,276 @@
+﻿using LarsProjekt.Database;
+using LarsProjekt.Database.Repositories;
+using LarsProjekt.Domain;
+using LarsProjekt.Models;
+using LarsProjekt.Models.Mapping;
+using LarsProjekt.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace LarsProjekt.Controllers;
+public class OrderController : Controller
+{
+    private readonly IOrderRepository _orderRepository;
+    private readonly IOrderDetailRepository _orderDetailRepository;
+    private readonly IUserRepository _userRepository;
+    private readonly IAddressRepository _addressRepository;
+    private readonly ICouponRepository _couponRepository;
+    private readonly ISqlUnitOfWork _sqlUnitOfWork;
+    public OrderController
+        (IOrderRepository orderRepository,
+        IOrderDetailRepository orderDetailRepository,
+        IUserRepository userRepository,
+        IAddressRepository addressRepository,
+        ICouponRepository couponRepository,
+        ISqlUnitOfWork sqlUnitOfWork)
+    {
+        _orderRepository = orderRepository;
+        _orderDetailRepository = orderDetailRepository;
+        _userRepository = userRepository;
+        _addressRepository = addressRepository;
+        _couponRepository = couponRepository;
+        _sqlUnitOfWork = sqlUnitOfWork;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var user = _userRepository.GetByName(HttpContext.User.Identity.Name);
+        var address = _addressRepository.Get(user.AddressId);
+        var orders = _orderRepository.GetAll();
+        var list = new List<OrderModel>();
+        foreach (var order in orders)
+        {
+            if (user.Id == order.UserId)
+            {
+                list.Add(order.ToModel());
+                order.Address = address;
+            }
+        }
+        return View(list);
+    }
+
+    [HttpGet]
+    public IActionResult Checkout()
+    {
+        var user = _userRepository.GetByName(HttpContext.User.Identity.Name);
+        var model = _addressRepository.Get(user.AddressId).ToModel();
+        return View(model);
+    }
+
+    [HttpGet]
+    public IActionResult Confirmation()
+    {
+        var shoppingCartItems = GetCartModels();
+        var offers = GetOfferModels();
+        var total = GetTotal();
+
+        if (offers.Count != 0)
+        {
+            total = offers.MinBy(x => x.DiscountPrice).DiscountPrice;
+        }
+
+        OrderConfirmationVM orderConfirmationVM = new OrderConfirmationVM()
+        {
+            Coupons = new CouponModel(),
+            Items = shoppingCartItems,
+            Total = total,
+            Offers = offers
+        };
+
+        return View(orderConfirmationVM);
+    }
+
+    [HttpGet]
+    public IActionResult Details(long id)
+    {
+        var detail = _orderDetailRepository.GetListWithOrderId(id);
+        var list = new List<OrderDetailModel>();
+        foreach (var item in detail)
+        {
+            list.Add(new OrderDetailModel
+            {
+                OrderId = item.OrderId,
+                ProductId = item.ProductId,
+                Quantity = item.Quantity,
+                UnitPrice = item.UnitPrice,
+                Id = id
+            });
+        }
+        return View(list);
+    }
+
+
+    [HttpPost]
+    public IActionResult CreateOrder(OrderModel model)
+    {
+
+        if (ModelState.IsValid)
+        {
+            var user = _userRepository.GetByName(HttpContext.User.Identity.Name);
+            var shoppingCartItems = GetCartModels();
+            var offers = GetOfferModels();
+            var total = GetTotal();
+
+            if (offers.Count != 0)
+            {
+                total = offers.MinBy(x => x.DiscountPrice).DiscountPrice;
+            }
+            // add order
+            var order = model.ToDomain();
+            order.UserId = user.Id;
+            order.AddressId = user.AddressId;
+            order.Address = user.Address;
+            order.Total = total;
+            _sqlUnitOfWork.OrderRepository.Add(order);
+            _sqlUnitOfWork.SaveChanges();
+
+            //add orderDetail            
+            foreach (var item in shoppingCartItems)
+            {
+                var orderDetail = new OrderDetail
+                {
+                    Quantity = item.Amount,
+                    UnitPrice = item.PriceOffer,
+                    ProductId = item.ProductId,
+                    OrderId = order.Id
+                };
+                _sqlUnitOfWork.OrderDetailRepository.Add(orderDetail);
+                _sqlUnitOfWork.SaveChanges();
+            }
+
+            // clear cart
+            var newCookieValue = JsonSerializer.Serialize(shoppingCartItems);
+            var newCookieValueOffer = JsonSerializer.Serialize(offers);
+            var options = new CookieOptions
+            {
+                Expires = DateTime.UtcNow.AddDays(-1)
+            };
+            Response.Cookies.Append("cookieShoppingCartItems", newCookieValue, options);
+            Response.Cookies.Append("cookieOffer", newCookieValueOffer, options);
+        }
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    public IActionResult Redeem(string couponCode)
+    {
+        Coupon coupon = _couponRepository.Get(couponCode);
+        decimal? total = GetDiscountPrice();
+        List<OfferModel>? offers = GetOfferModels();
+
+        var offer = offers.FirstOrDefault(x => x.CouponCode == couponCode);
+
+        if(offers.Count() > 3)
+        {
+            var discountPrice = GetDiscountPrice();
+            string message = "You can use a maximum of 3 codes per order.";
+            return Json(new { success = "false", message = message, discountPrice = discountPrice });
+        }
+        if (offer == null)
+        {
+            if (total > 0)
+            {
+                var discount = coupon.Discount;
+                decimal.TryParse(discount, out decimal x);
+                var totalDiscount = (x / 100) * total;
+                var newTotalPrice = total - totalDiscount;
+                total = newTotalPrice;
+            }
+            var model = new OfferModel
+            {
+                CouponCode = couponCode,
+                DiscountPrice = total,
+                Id = offers.Count > 0 ? offers.Max(o => o.Id) + 1 : 1
+            };
+            offers.Add(model);
+            var newCookieValue = JsonSerializer.Serialize(offers);
+            var options = new CookieOptions
+            {
+                Expires = DateTime.UtcNow.AddDays(1)
+            };
+            Response.Cookies.Append("cookieOffers", newCookieValue, options);
+
+            return Json(new { success = "true", total = total });
+        }
+        else
+        {
+            var discountPrice = GetDiscountPrice();
+            string message = "This code can only be used once for an order.";
+            return Json(new { success = "false", message = message, discountPrice = discountPrice });
+        }
+        
+        //TODO in domain objekt auslagern
+    }
+
+    [HttpDelete]
+    public IActionResult RemoveCoupon(long id)
+    {
+        var offers = GetOfferModels();
+
+        var offer = offers.FirstOrDefault(x => x.Id == id);
+        if (offer != null)
+        {
+            offers.Remove(offer);
+        }
+
+        var newCookieValue = JsonSerializer.Serialize(offers);
+        Response.Cookies.Append("cookieOffers", newCookieValue);
+        return Ok(new { success = "true"});
+    }
+
+    public IActionResult Payment()
+    {
+        return View();
+    }
+
+    private decimal? GetTotal()
+    {
+        var shoppingCartItems = GetCartModels();
+
+        decimal? total = (from cartItems in shoppingCartItems
+                          select cartItems.Amount *
+                          cartItems.PriceOffer).Sum();
+
+        return total ?? decimal.Zero;
+    }
+
+    private decimal? GetDiscountPrice()
+    {
+        var offers = GetOfferModels();
+        var total = GetTotal();
+        var y = offers.MinBy(x => x.DiscountPrice);
+        if (y != null)
+        {
+            total = y.DiscountPrice;
+        }
+        return total;
+    }
+
+    private List<OfferModel>? GetOfferModels()
+    {
+        var offers = new List<OfferModel>();
+        var cookieValue = Request.Cookies["cookieOffers"];
+
+        if (!string.IsNullOrWhiteSpace(cookieValue))
+        {
+            offers = JsonSerializer.Deserialize<List<OfferModel>>(cookieValue);
+        }
+        return offers;
+    }
+
+    private List<ShoppingCartItemModel>? GetCartModels()
+    {
+        var shoppingCartItems = new List<ShoppingCartItemModel>();
+        var cookieValue = Request.Cookies["cookieShoppingCartItems"];
+
+        if (!string.IsNullOrWhiteSpace(cookieValue))
+        {
+            shoppingCartItems = JsonSerializer.Deserialize<List<ShoppingCartItemModel>>(cookieValue);
+        }
+        return shoppingCartItems;
+    }
+
+}
+
+
